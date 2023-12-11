7e7441696294ad768c88ea41f7c3a85efd41396c
Doug Keller <dg.kllr.jr@gmail.com>
2023-12-11 22:55:26 +0100

    updated to 0.0.8; added a bunch of stuff, a delete command, fixed the git command, made git commits on log and delete

--- douglog/douglog.py
+++ douglog/douglog.py
@@ -5,6 +5,7 @@ import os
 import time
 import re
 import numpy as np
+from glob import glob
 import subprocess
 from datetime import datetime
 
@@ -18,8 +19,8 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 #  ──────────────────────────────────────────────────────────────────────────
 # global functions
 
-def logbook_not_found(name):
-    click.echo(name + ' logbook not found in config.')
+def logbook_not_found(logbook):
+    click.echo(logbook + ' logbook not found in config.')
     exit()
 
 #  ──────────────────────────────────────────────────────────────────────────
@@ -76,13 +77,13 @@ def dlog(ctx, config):
 # log command to generate single logs
 
 @dlog.command('log', short_help='Open a new log in your editor.',context_settings=CONTEXT_SETTINGS)
-@click.argument('name', type=str)
+@click.argument('logbook', type=str)
 @click.pass_context
-def log(ctx, name):
-    """Open a new log in the NAME's logbook in your editor."""
+def log(ctx, logbook):
+    """Open a new log in LOGBOOK in your editor."""
 
-    if name in ctx.obj['logbooks']:
-        logbook_path = ctx.obj['home'] / Path(name)
+    if logbook in ctx.obj['logbooks']:
+        logbook_path = ctx.obj['home'] / Path(logbook)
 
         epoch = str(np.floor(time.time()).astype(np.int64))
         date = time.strftime("%a, %d %b %Y %H:%M:%S UTC", time.gmtime())
@@ -91,21 +92,39 @@ def log(ctx, name):
         with open(file_path, 'a') as file:
             file.write('# ' + date + '\n')
 
+        # editting 
         click.edit(filename=file_path, editor=ctx.obj['editor'])
 
+        # committing 
+        git_path = logbook + '/' + file_path.name
+        ctx.invoke(git, git_commands=['add', git_path])
+        ctx.invoke(git, git_commands=['commit', '-m', '"Added ' + git_path + '"'])
+
     else:
-        logbook_not_found(name)
+        logbook_not_found(logbook)
 
 #  ──────────────────────────────────────────────────────────────────────────
 # list command to list logs
 
 @dlog.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-l', '--logs', default=False, help='List logs as well.', is_flag=True)
 @click.pass_context
-def list(ctx):
+def list(ctx, logs):
     """Lists all logbooks."""
 
     for logbook in ctx.obj['logbooks']:
         click.echo(logbook)
+        
+        if logs:
+            logbook_path = ctx.obj['home'] / Path(logbook)
+            file_paths = sorted(logbook_path.glob('*.md'))
+
+            for file_path in file_paths:
+
+                date = datetime.utcfromtimestamp(int(file_path.stem)).isoformat()
+                click.echo(file_path.name + ' : ' + date)
+
+            click.echo()
 
     exit()
 
@@ -113,19 +132,19 @@ def list(ctx):
 # search command
 
 @dlog.command('search', short_help='Searches through your logs.', context_settings=CONTEXT_SETTINGS)
-@click.option('-n', '--name', type=str, help="Specified logbook to search.", default=None)
+@click.option('-l', '--logbook', type=str, help="Specified logbook to search.", default=None)
 @click.argument('regex', type=str)
 @click.pass_context
-def search(ctx, regex, name):
+def search(ctx, regex, logbook):
     """
     Searches through your logs for the given REGEX. Searches through all logs unless one is specified. Results are printed as:
 
     logbook: filename: date: line number: line
     """
 
-    def inbook(ctx, name, regex):
+    def inbook(ctx, logbook, regex):
 
-        logbook_path = ctx.obj['home'] / Path(name)
+        logbook_path = ctx.obj['home'] / Path(logbook)
         file_paths = sorted(logbook_path.glob('*.md'))
 
         for file_path in file_paths:
@@ -137,25 +156,28 @@ def search(ctx, regex, name):
                 for i, line in enumerate(file):
 
                     if re.search(regex, line):
-                        found_line = name + ': ' + file_path.stem + ': ' + date + ': ' + str(i + 1) + ': ' + line.strip('\n')
+                        found_line = logbook + ': ' + file_path.stem + ': ' + date + ': ' + str(i + 1) + ': ' + line.strip('\n')
                         click.echo(found_line)
 
 
-    if name:
-        if name in ctx.obj['logbooks']:
-            inbook(ctx, name, regex)
+    if logbook:
+        if logbook in ctx.obj['logbooks']:
+            inbook(ctx, logbook, regex)
         else:
-            logbook_not_found(name)
+            logbook_not_found(logbook)
 
     else:
-        for name in ctx.obj['logbooks']:
-            inbook(ctx, name, regex)
+        for logbook in ctx.obj['logbooks']:
+            inbook(ctx, logbook, regex)
 
 #  ──────────────────────────────────────────────────────────────────────────
 # git command
 
-@dlog.command('git', short_help='Git manage the logbooks.', context_settings=CONTEXT_SETTINGS)
-@click.argument('git_commands', type=str, nargs=-1)
+CONTEXT_SETTINGS_git = CONTEXT_SETTINGS.copy()
+CONTEXT_SETTINGS_git['ignore_unknown_options'] = True
+
+@dlog.command('git', short_help='Git manage the logbooks.', context_settings=CONTEXT_SETTINGS_git)
+@click.argument('git_commands', type=click.UNPROCESSED, nargs=-1)
 @click.pass_context
 def git(ctx, git_commands):
     """Git command to manage the douglog git repository."""
@@ -166,3 +188,39 @@ def git(ctx, git_commands):
         command.append(arg)
 
     subprocess.run(command)
+
+#  ──────────────────────────────────────────────────────────────────────────
+# delete command
+
+@dlog.command('delete', short_help='Delete a log.', context_settings=CONTEXT_SETTINGS)
+@click.argument('logbook', type=str)
+@click.option('-l', '--log', type=str, default=None, help='Delete specified log.')
+@click.pass_context
+def delete(ctx, logbook, log):
+    """Delete a log from the LOGBOOK. Deletes the most recent log if none is specified."""
+
+    if logbook in ctx.obj['logbooks']:
+        logbook_path = ctx.obj['home'] / Path(logbook)
+        file_paths = sorted(logbook_path.glob('*.md'))
+
+        if log:
+            log_path = logbook_path / Path(log)
+            if log_path in file_paths:
+                delete_log = log_path
+            else:
+                click.echo('Log specified but not found.')
+                exit()
+        else:
+            delete_log = file_paths[-1]
+
+        if click.confirm('Do you want to delete: ' + logbook + '/' + delete_log.name + '?'):
+            click.echo('Deleting ' + delete_log.name + ' in ' + logbook)
+            subprocess.run(['rm', delete_log])
+
+            # committing 
+            git_path = logbook + '/' + delete_log.name
+            ctx.invoke(git, git_commands=['add', git_path])
+            ctx.invoke(git, git_commands=['commit', '-m', '"Deleted ' + git_path + '"'])
+
+    else:
+        logbook_not_found(logbook)
